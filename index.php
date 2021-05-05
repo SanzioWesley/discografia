9df57ecb952dbe33d92bd2d6ec6a43a0c662eabc
sanziowesley <sanziowesley@hotmail.com>
2021-05-05 20:12:53 -0300

    criando layout

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Discografia</title>
+    <!-- jquery - link cdn -->
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <!-- bootstrap - link cdn -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css" />
+    <script>
+        // código javascript
+    </script>
+</head>
+
+<body>
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <img src="imagens/logo.png" />
+            </div>
+
+            <div>
+                <ul class="nav navbar-nav navbar-right">
+                    <h1>Discografia</h1>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+    </div>
+
+    <div class="navbar">
+        <form class="form-inline">
+            <h3 style="padding-right: 400px;">Digite uma palavra passe</h3> <br>
+            <input class="btn btn-custom btn-search btn-lg" type="search"  aria-label="Pesquisar">
+            <button class="btn btn-custom btn-blue btn-lg" type="submit">Procurar</button>
+        </form>
+    </div>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
