9118e9c8792f7795dfb770706a4a9a49ccb87f2a
sanziowesley <sanziowesley@hotmail.com>
2021-05-11 21:50:12 -0300

    Selecionando album do mysql 2

--- exibir_album.php
+++ exibir_album.php
@@ -1,27 +1,27 @@
 <?php
 
-require_once('db.php');
+    require_once('db.php');
 
-$sql = " SELECT * FROM album ";
+    $sql = " SELECT * FROM album ";
 
-$objDB = new db();
-$link = $objDB -> conecta_mysql();
+    $objDB = new db();
+    $link = $objDB -> conecta_mysql();
 
-$resultado_id = mysqli_query($link, $sql);
+    $resultado_id = mysqli_query($link, $sql);
 
 
-if($resultado_id){
-    $dados_album = array();
+    if($resultado_id){
+        $dados_album = array();
 
-    while($linha= mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)){
-        $dados_album[] = $linha;
+        while($linha= mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)){
+            $dados_album[] = $linha;
+        }
+
+        var_dump($dados_album);
+
+    } else{
+        echo 'Erro ao exibir o álbum';
     }
-
-    var_dump($dados_album);
-
-} else{
-    echo 'Erro ao exibir o álbum';
-}
  
 ?>
  
